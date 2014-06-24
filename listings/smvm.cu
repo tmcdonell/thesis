e195cd5c30672c86ff8478ac055f07054237fe8a
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-06-24 18:29:58 +1000

    update smvm listing

--- listings/smvm.cu
+++ listings/smvm.cu
@@ -1,15 +1,16 @@
 #include <accelerate_cuda.h>
 extern "C" __global__ void foldSeg
 (
-    const DIM1 shIn0,
-    const Int32* __restrict__ arrIn0_a0,
-    const DIM1 shIn1,
-    const float* __restrict__ arrIn1_a0,
-    const DIM1 shIn2,
-    const Int32* __restrict__ arrIn2_a1,
-    const float* __restrict__ arrIn2_a0,
-    const DIM1 shOut,
-    float* __restrict__ arrOut_a0)
+    const Int64 shIn0_0,
+    const float* __restrict__ arrIn0_0,
+    const Int64 shIn1_0,
+    const Int32* __restrict__ arrIn1_1,
+    const float* __restrict__ arrIn1_0,
+    const Int64 shIn2_0,
+    const Int32* __restrict__ arrIn2_0,
+    const Int64 shOut_0,
+    float* __restrict__ arrOut_0
+)
 {
     const int vectors_per_block = blockDim.x / warpSize;
     const int num_vectors = vectors_per_block * gridDim.x;
@@ -17,24 +18,22 @@ extern "C" __global__ void foldSeg
     const int vector_id = thread_id / warpSize;
     const int thread_lane = threadIdx.x & warpSize - 1;
     const int vector_lane = threadIdx.x / warpSize;
-    const int num_segments = indexHead(shOut);
-    const int total_segments = size(shOut);
+    const int num_segments = shOut_0;
+    const int total_segments = shOut_0;
     int seg;
     int ix;
     extern volatile __shared__ int s_ptrs[][2];
     volatile float* sdata0 = (float*) &s_ptrs[vectors_per_block][2];
     float x0;
     float y0;
-    const Int64 sh0 = shIn2;
+    const Int64 sh0 = shIn1_0;
 
     for (seg = vector_id; seg < total_segments; seg += num_vectors) {
         const int s = seg % num_segments;
         const int base = seg / num_segments * sh0;
 
         if (thread_lane < 2) {
-            const int v8 = s + thread_lane;
-
-            s_ptrs[vector_lane][thread_lane] = arrIn0_a0[v8];
+            s_ptrs[vector_lane][thread_lane] = arrIn2_0[s + thread_lane];
         }
 
         const int start = base + s_ptrs[vector_lane][0];
@@ -44,20 +43,16 @@ extern "C" __global__ void foldSeg
         if (num_elements > warpSize) {
             ix = start - (start & warpSize - 1) + thread_lane;
             if (ix >= start) {
-                const Int64 v3 = ix;
-                const int v4 = toIndex(shIn2, shape(v3));
-                const int v5 = toIndex(shIn1, shape((Int64) arrIn2_a1[v4]));
-                const int v6 = toIndex(shIn2, shape(v3));
+                const Int64 v2 = ({ assert(ix >= 0 && ix < shIn1_0); ix; });
+                const Int64 v3 = (Int64) arrIn1_1[v2];
 
-                y0 = arrIn1_a0[v5] * arrIn2_a0[v6];
+                y0 = arrIn0_0[v3] * arrIn1_0[v2];
             }
             if (ix + warpSize < end) {
-                const Int64 v3 = ix + warpSize;
-                const int v4 = toIndex(shIn2, shape(v3));
-                const int v5 = toIndex(shIn1, shape((Int64) arrIn2_a1[v4]));
-                const int v6 = toIndex(shIn2, shape(v3));
+                const Int64 v2 = ({ assert(ix + warpSize >= 0 && ix + warpSize < shIn1_0); ix + warpSize; });
+                const Int64 v3 = (Int64) arrIn1_1[v2];
 
-                x0 = arrIn1_a0[v5] * arrIn2_a0[v6];
+                x0 = arrIn0_0[v3] * arrIn1_0[v2];
                 if (ix >= start) {
                     y0 = x0 + y0;
                 } else {
@@ -65,21 +60,17 @@ extern "C" __global__ void foldSeg
                 }
             }
             for (ix += 2 * warpSize; ix < end; ix += warpSize) {
-                const Int64 v3 = ix;
-                const int v4 = toIndex(shIn2, shape(v3));
-                const int v5 = toIndex(shIn1, shape((Int64) arrIn2_a1[v4]));
-                const int v6 = toIndex(shIn2, shape(v3));
+                const Int64 v2 = ({ assert(ix >= 0 && ix < shIn1_0); ix; });
+                const Int64 v3 = (Int64) arrIn1_1[v2];
 
-                x0 = arrIn1_a0[v5] * arrIn2_a0[v6];
+                x0 = arrIn0_0[v3] * arrIn1_0[v2];
                 y0 = x0 + y0;
             }
         } else if (start + thread_lane < end) {
-            const Int64 v3 = start + thread_lane;
-            const int v4 = toIndex(shIn2, shape(v3));
-            const int v5 = toIndex(shIn1, shape((Int64) arrIn2_a1[v4]));
-            const int v6 = toIndex(shIn2, shape(v3));
+            const Int64 v2 = ({ assert(start + thread_lane >= 0 && start + thread_lane < shIn1_0); start + thread_lane; });
+            const Int64 v3 = (Int64) arrIn1_1[v2];
 
-            y0 = arrIn1_a0[v5] * arrIn2_a0[v6];
+            y0 = arrIn0_0[v3] * arrIn1_0[v2];
         }
         ix = min(num_elements, warpSize);
         sdata0[threadIdx.x] = y0;
@@ -120,7 +111,8 @@ extern "C" __global__ void foldSeg
             } else {
                 y0 = 0.0f;
             }
-            arrOut_a0[seg] = y0;
+            arrOut_0[seg] = y0;
         }
     }
 }
+
